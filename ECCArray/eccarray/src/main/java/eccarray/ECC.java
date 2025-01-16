b9f721db5e0d22a871a4274bdb309eb4abf2137f
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-01-16 17:28:28 +0700

    Setting to VPS

---
+++ ECCArray/eccarray/src/main/java/eccarray/ECC.java
@@ -0,0 +1,45 @@
+package eccarray;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.spec.ECGenParameterSpec;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+
+public class ECC {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static KeyPair generateKeyPair() throws GeneralSecurityException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC", "BC");
+        // keyPairGenerator.initialize(384);
+        keyPairGenerator.initialize(new ECGenParameterSpec("secp256r1"));
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    public static byte[] encrypt(String message, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("ECIES", "BC");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(message.getBytes());
+    }
+
+    public static String decrypt(String encryptedMessage, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("ECIES", "BC");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+        return new String(decryptedBytes);
+    }
+    // public static PublicKey decodePublicKey(byte[] keyBytes, String algorithm) throws Exception {
+    //     X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+    //     KeyFactory keyFactory = KeyFactory.getInstance(algorithm); // Misalnya "EC"
+    //     return keyFactory.generatePublic(keySpec);
+    // }
+
+}
---
+++ ECDH/ecdh/src/main/java/ecdh/Server.java
@@ -0,0 +1,5 @@
+package eccarray;
+
+public class Server {
+    
+}
