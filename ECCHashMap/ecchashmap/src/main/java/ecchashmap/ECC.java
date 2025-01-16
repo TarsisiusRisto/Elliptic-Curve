6b876a121a4280a2ee48185370102007898e616e
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-01-16 20:11:17 +0700

    Finish

--- ECCHashMap/ecchashmap/src/main/java/ecchashmap/ECC.java
+++ ECCHashMap/ecchashmap/src/main/java/ecchashmap/ECC.java
@@ -21,7 +21,7 @@ public class ECC {
 
     public static KeyPair generateKeyPair() throws GeneralSecurityException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC", "BC");
-        keyPairGenerator.initialize(new ECGenParameterSpec("secp384r1"));
+        keyPairGenerator.initialize(new ECGenParameterSpec("secp521r1"));
         // keyPairGenerator.initialize(384);
         return keyPairGenerator.generateKeyPair();
     }
